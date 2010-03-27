9b57072909d0bd904e6b32a248baba98077bb096
Gintautas Miliauskas <gintautas@miliauskas.lt>
2010-03-27 12:54:02 +0200

    Removed references to Shrubbery Software.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2009 Shrubbery Software
+# Copyright (c) 2009-2010 Gintautas Miliauskas
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
